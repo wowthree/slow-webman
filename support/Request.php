d7259903fa552aecfa072fc842164262cd3da9b6
空空 <wow3ter@163.com>
2023-03-02 20:27:37 +0800

    feat: sessionId 获取问题

--- support/Request.php
+++ support/Request.php
@@ -16,6 +16,8 @@
 namespace support;
 
 use Illuminate\Support\Str;
+use Workerman\Protocols\Http\Session;
+use Workerman\Worker;
 
 /**
  * Class Request
@@ -70,5 +72,34 @@ class Request extends \Webman\Http\Request
     public function __get($name)
     {
         return $this->input($name);
+    }
+
+    /**
+     * 覆盖原方法
+     * 原方法存在preg_match参数问题，
+     * @param $session_id
+     * @return string
+     */
+    public function sessionId($session_id = null)
+    {
+        $session_name = Session::$name;
+        $sid = $session_id ? '' : $this->cookie($session_name);
+        if ($sid === '' || $sid === null) {
+            if ($this->connection === null) {
+                Worker::safeEcho('Request->session() fail, header already send');
+                return false;
+            }
+            $sid = $session_id ? $session_id : static::createSessionId();
+            $cookie_params = Session::getCookieParams();
+            $this->connection->__header['Set-Cookie'] = array($session_name . '=' . $sid
+                . (empty($cookie_params['domain']) ? '' : '; Domain=' . $cookie_params['domain'])
+                . (empty($cookie_params['lifetime']) ? '' : '; Max-Age=' . $cookie_params['lifetime'])
+                . (empty($cookie_params['path']) ? '' : '; Path=' . $cookie_params['path'])
+                . (empty($cookie_params['samesite']) ? '' : '; SameSite=' . $cookie_params['samesite'])
+                . (!$cookie_params['secure'] ? '' : '; Secure')
+                . (!$cookie_params['httponly'] ? '' : '; HttpOnly'));
+        }
+
+        return $sid;
     }
 }
